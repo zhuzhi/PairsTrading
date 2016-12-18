63f97d548c27fc54572d6390270fd99e87a50970
zhuzhi <zhuzhi90@gmail.com>
2016-12-19 00:50:10 +0800

    init

---
+++ R/data.R
@@ -0,0 +1,75 @@
+#' 农业银行(601288)数据
+#'
+#' 数据来源自新浪股票，包含复权因子。
+#'
+#' @format 数据格式为 data frame，有8个变量:
+#' \describe{
+#'   \item{date}{交易日期}
+#'   \item{open}{开盘价}
+#'   \item{high}{最高价}
+#'   \item{low}{最低价}
+#'   \item{close}{收盘价}
+#'   \item{volume}{交易量}
+#'   \item{amount}{交易金额}   
+#'   \item{factor}{复权因子}
+#'   ...
+#' }
+#' @source \url{http://finance.sina.com.cn/stock/}
+"sh601288"
+
+#' 工商银行(601398)数据
+#'
+#' 数据来源自新浪股票，包含复权因子。
+#'
+#' @format 数据格式为 data frame，有8个变量:
+#' \describe{
+#'   \item{date}{交易日期}
+#'   \item{open}{开盘价}
+#'   \item{high}{最高价}
+#'   \item{low}{最低价}
+#'   \item{close}{收盘价}
+#'   \item{volume}{交易量}
+#'   \item{amount}{交易金额}   
+#'   \item{factor}{复权因子}
+#'   ...
+#' }
+#' @source \url{http://finance.sina.com.cn/stock/}
+"sh601398"
+
+#' 建设银行(601939)数据
+#'
+#' 数据来源自新浪股票，包含复权因子。
+#'
+#' @format 数据格式为 data frame，有8个变量:
+#' \describe{
+#'   \item{date}{交易日期}
+#'   \item{open}{开盘价}
+#'   \item{high}{最高价}
+#'   \item{low}{最低价}
+#'   \item{close}{收盘价}
+#'   \item{volume}{交易量}
+#'   \item{amount}{交易金额}   
+#'   \item{factor}{复权因子}
+#'   ...
+#' }
+#' @source \url{http://finance.sina.com.cn/stock/}
+"sh601939"
+
+#' 中国银行(601988)数据
+#'
+#' 数据来源自新浪股票，包含复权因子。
+#'
+#' @format 数据格式为 data frame，有8个变量:
+#' \describe{
+#'   \item{date}{交易日期}
+#'   \item{open}{开盘价}
+#'   \item{high}{最高价}
+#'   \item{low}{最低价}
+#'   \item{close}{收盘价}
+#'   \item{volume}{交易量}
+#'   \item{amount}{交易金额}   
+#'   \item{factor}{复权因子}
+#'   ...
+#' }
+#' @source \url{http://finance.sina.com.cn/stock/}
+"sh601988"
